0f5d5dac1ce3134ce2d2a6dd034052ffe3f6c4d6
xavier <xavier@tttp.eu>
2015-03-23 10:48:04 +0100

    test the hook for now

---
+++ privatefields.php
@@ -0,0 +1,125 @@
+<?php
+
+require_once 'privatefields.civix.php';
+
+function privatefields_civicrm_pageRun(&$page) {
+  $pageName = $page->getVar('_name');
+print_r($page);
+die ($pageName);
+  if ($pageName == 'CRM_Admin_Page_Tag') {
+    /*
+     * retrieve all tag enhanced data and put in array with tag_id as index
+     */
+    $enhancedTags = CRM_Enhancedtags_BAO_TagEnhanced::getValues(array());
+    $coordinators = array();
+    foreach ($enhancedTags as $enhancedTag) {
+      $coordinators[$enhancedTag['tag_id']] = CRM_Enhancedtags_BAO_TagEnhanced::getCoordinatorName($enhancedTag['coordinator_id']);
+    }
+    $page->assign('coordinators', $coordinators);
+  }
+}
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function privatefields_civicrm_config(&$config) {
+  _privatefields_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function privatefields_civicrm_xmlMenu(&$files) {
+  _privatefields_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function privatefields_civicrm_install() {
+  _privatefields_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function privatefields_civicrm_uninstall() {
+  _privatefields_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function privatefields_civicrm_enable() {
+  _privatefields_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function privatefields_civicrm_disable() {
+  _privatefields_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function privatefields_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _privatefields_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function privatefields_civicrm_managed(&$entities) {
+  _privatefields_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function privatefields_civicrm_caseTypes(&$caseTypes) {
+  _privatefields_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function privatefields_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _privatefields_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
